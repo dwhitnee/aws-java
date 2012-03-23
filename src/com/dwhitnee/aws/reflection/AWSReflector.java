347cab81709b6c4c050263413f993dcc41a896ad
David Whitney <dwhitnee@gmail.com>
2012-03-23 00:10:45 -0700

    Call with json

--- src/com/dwhitnee/aws/reflection/AWSReflector.java
+++ src/com/dwhitnee/aws/reflection/AWSReflector.java
@@ -9,10 +9,13 @@ import com.amazonaws.services.dynamodb.AmazonDynamoDBClient;
 
 import com.amazonaws.services.dynamodb.model.AttributeValue;
 import com.amazonaws.services.dynamodb.model.DescribeTableRequest;
+import com.amazonaws.services.dynamodb.model.DescribeTableResult;
 import com.amazonaws.services.dynamodb.model.ScanRequest;
 import com.amazonaws.services.dynamodb.model.ScanResult;
 import com.amazonaws.services.dynamodb.model.TableDescription;
 import com.amazonaws.services.dynamodb.model.TableStatus;
+
+import com.google.gson.Gson;
 
 
 public class AWSReflector {
@@ -24,30 +27,60 @@ public class AWSReflector {
 
         System.out.println("Hello World");
 
-        DescribeTableRequest describeTableRequest =
-            new DescribeTableRequest().withTableName("Customers");
-
-        TableDescription tableDescription =
-            dynamoDB.describeTable( describeTableRequest ).getTable();
-
-        System.out.println("Table Description: " + tableDescription);
+        System.out.println("Output: " + callDynDynamo());
     }
 
 
-    private static void init() throws Exception {
+    static void init() throws Exception {
 
         AWSCredentials credentials = new PropertiesCredentials(
             ClassLoader.getSystemClassLoader().
             getResourceAsStream("AwsCredentials.properties"));
 
-
-//         AWSCredentials credentials = new PropertiesCredentials(
-//             AWSReflector.class.getResourceAsStream(
-//                 "AwsCredentials.properties"));
-
         dynamoDB = new AmazonDynamoDBClient(credentials);
     }
 
+
+
+    static void callDynamo() {
+        Gson gson = new Gson();
+
+        DescribeTableRequest req =
+            new DescribeTableRequest().withTableName("Customers");
+
+        DescribeTableResult resp = dynamoDB.describeTable( req );
+
+        TableDescription table = resp.getTable();
+
+        System.out.println("Request: " + gson.toJson( req ) );
+        System.out.println("Response: " + gson.toJson( resp ) );
+
+        // System.out.println("Table Description: " + table );
+        // System.out.println("Table JSON: " + gson.toJson( table ));
+
+
+        // MyType target2 = gson.fromJson(json, MyType.class);
+    }
+
+
+    //----------------------------------------------------------------------
+    static String callDynDynamo() {
+
+        Gson gson = new Gson();
+
+        String json = "{\"tableName\":\"Customers\"}";
+
+        DescribeTableRequest req =
+            gson.fromJson( json, DescribeTableRequest.class);
+
+        DescribeTableResult resp = dynamoDB.describeTable( req );
+
+        TableDescription table = resp.getTable();
+
+        return gson.toJson( resp );
+
+        // MyType target2 = gson.fromJson(json, MyType.class);
+    }
 
 
 }
